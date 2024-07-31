31faef33fa625522af3c2c563971210caf81999c
RadhityaOben <radhityaoben@gmail.com>
2024-07-31 13:11:40 +0700

    Change default port

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
 
+var port = process.env.PORT || 1054;
+app.listen(port, () => {});
+
 module.exports = app;
