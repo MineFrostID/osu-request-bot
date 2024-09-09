3a5ee1136820aba2f9b933117553b609a45c5208
MineFrostID <obenunity56@gmail.com>
2024-09-09 10:36:30 +0700

    Change response message to chat

--- routes/index.js
+++ routes/index.js
@@ -2,96 +2,51 @@ var express = require("express");
 var router = express.Router();
 const { v2, auth } = require("osu-api-extended");
 const banchojs = require("bancho.js");
-const account = require("../users/account.json");
-const config = require("../config/config.json");
+const account = require("../config/account.json");
 
+const USERNAME = account.username;
 const CODE = account.token;
-const CLIENT_ID = config.CLIENT_ID;
-const CLIENT_SECRET = config.CLIENT_PASSWORD;
-const REDIRECT_URI = config.REDIRECT_URL;
-const SCOPE_LIST = config.SCOPE_LIST;
+const PASSWORD = account.password;
 
-let client, users, userInfo;
+let client, users;
 
-let loginStatus = false;
-
-// Welcome Message
-const welcomeMessage = () => {
+// Banchojs client
+try {
+  client = new banchojs.BanchoClient({
+    username: USERNAME,
+    password: CODE,
+  });
+} catch (e) {
   console.log("=====================================");
-  console.log("WELCOME TO OSU! REQUEST YOUTUBE BOT!");
+  console.log("Failed to login");
+  console.log("Please check your credential in config/account.json");
   console.log("=====================================");
-  console.log("Please login to your osu! account");
-  console.log("=====================================");
-  console.log("Open this link to login:");
-  console.log("http://localhost:3000/login");
-  console.log("=====================================");
-};
-welcomeMessage();
+  return;
+}
 
-// Banchojs Login
-const loginBanchoJs = async (username) => {
-  if (loginStatus) return;
-  try {
-    client = new banchojs.BanchoClient({
-      username: username,
-      password: CODE,
-    });
-
-    users = client.getSelf();
-    await client.connect();
-
+// Login to osu-api-extended and banchojs
+const login = async () => {
+  // // Auth via lazer
+  const login_lazer = await auth.login_lazer(USERNAME, PASSWORD);
+  if (login_lazer.access_token === undefined) {
     console.log("=====================================");
-    console.log("BANCHOJS CONNECTED!");
+    console.log("Failed to login");
+    console.log("Please check your credential in config/account.json");
     console.log("=====================================");
-  } catch (e) {
-    console.log("=====================================");
-    console.log("Failed to connect BanchoJS");
-    console.log("Please check your credential in users/account.json");
-    console.log(
-      "Get your token at https://osu.ppy.sh/home/account/edit#legacy-api"
-    );
-    console.log("=====================================");
-    console.log("Stopping the server...");
-
-    setTimeout(() => {
-      process.exit();
-    }, 5000);
+    return;
   }
-};
 
-const url = async () => {
-  const url = auth.build_url(CLIENT_ID, REDIRECT_URI, SCOPE_LIST);
-  return url;
-};
-
-const login = async (data) => {
-  if (loginStatus) return;
-  const token = data.query.code;
-
-  userInfo = await auth.authorize(
-    token,
-    "osu",
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-  );
-
-  await auth.login(CLIENT_ID, CLIENT_SECRET, SCOPE_LIST);
-
-  loginBanchoJs(userInfo.username);
+  users = client.getSelf();
+  await client.connect();
 
   console.log("=====================================");
-  console.log("Logged in as " + userInfo.username);
-  console.log("SUCCESS!");
+  console.log("Bot connected!");
   console.log("=====================================");
-  console.log();
-
-  loginStatus = true;
 };
+login();
 
 // Main function
 const main = async (req, req2) => {
-  if (!loginStatus) return false;
   // Get id and username from request
   const beatmapId = req;
   const username = req2;
@@ -115,6 +70,7 @@ const main = async (req, req2) => {
   const title = data.beatmapset.title;
   const respond = `${mapUrl} ${artist} - ${title}`;
   const message = `[${respond}]`;
+  const respondMessage = `Request send: ${artist} - ${title} [${mapUrl}]`;
 
   // Send message to osu!
   try {
@@ -130,7 +86,7 @@ const main = async (req, req2) => {
     console.log("Chat Sent to osu!: ");
     console.log(respond);
     console.log("=====================================");
-    return true;
+    return respondMessage;
   } catch (e) {
     console.log("=====================================");
     console.log("Failed to send message");
@@ -141,55 +97,21 @@ const main = async (req, req2) => {
 
 // GET home page
 router.get("/", function (req, res) {
-  if (loginStatus) {
-    res.send("Welcome to osu! Request Youtube Bot!");
-  } else if (!loginStatus) {
-    res.send(
-      "Welcome to osu! Request Youtube Bot! Please open this link to login: http://localhost:3000/login"
-    );
-  }
-});
-
-router.get("/login", async (req, res) => {
-  if (loginStatus) {
-    res.send("You are already logged in!");
-    return;
-  }
-  res.redirect(await url());
-});
-
-router.get("/callback", async (req, res) => {
-  if (loginStatus) {
-    res.send("You are already logged in!");
-    return;
-  }
-  await login(req);
-  res.send("Logged in! You can close this tab now!");
+  res.send("Welcome to osu! Request Youtube Bot!");
 });
 
 // Get request anonymously
-router.get("/request/:id", (req, res) => {
-  if (!loginStatus) {
-    res.send("Please login first!");
-    return;
-  }
-  main(req.params.id, "Anonymous");
-  res.send("Request Sent!");
+router.get("/request/:id", async (req, res) => {
+  const messageSend = await main(req.params.id, "Anonymous");
+  if (!messageSend) res.send("Beatmap not found, try another one!");
+  else res.send(messageSend);
 });
 
 // Get request with username
 router.get("/request/:id/:name", async (req, res) => {
-  if (!loginStatus) {
-    res.send("Please login first!");
-    return;
-  }
-  const status = await main(req.params.id, req.params.name);
-
-  if (status) {
-    res.send("Request Sent!");
-  } else {
-    res.send("Beatmap not found, try another one!");
-  }
+  const messageSend = await main(req.params.id, req.params.name);
+  if (!messageSend) res.send("Beatmap not found, try another one!");
+  else res.send(messageSend);
 });
 
 module.exports = router;
